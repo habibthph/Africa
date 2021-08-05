042414cbf4b57ef963e2be57972c95159478a3e7
Patrick Habib <Patrick.Habib@yahoo.com>
2021-08-05 06:54:03 -0400

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Patrick Habib on 8/5/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
